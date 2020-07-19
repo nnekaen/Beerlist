3ddc67f08c6d7463b82665ac55ea3228600f4799
uche <thesimplearchitect@gmail.com>
2020-07-19 11:18:05 +0200

    Fetch list of beers

---
+++ src/components/BeerList.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'BeerList',
+
+    data: () => ({
+      result: null
+    }),
+
+    beforeMount() {
+      fetch('https://api.punkapi.com/v2/beers?malt=extra_pale')
+        .then(response =>  response.json())
+        .then(data => this.result = data);
+    }
+  }
+</script>
